9e89706ace391920e297a02340fcc0adeaed66f4
Dejan Krstovski <tandolius@gmail.com>
2025-08-09 04:46:43 +0200

    Update FolderNames.java

--- src/persistence/serialization/FolderNames.java
+++ src/persistence/serialization/FolderNames.java
@@ -1,6 +1,1 @@
-package persistence.serialization;
 
-public interface FolderNames {
-	public static final String DATA_FOLDER = "C:\\Users\\DejanKrstovski\\eclipse-myworkspace\\Quiz\\QuizData";
-    public static final String THEME_FILE_PREFIX = DATA_FOLDER + "\\theme_";
-}
